a4f29d3aae465c05891a0ae6fdd3c5d250c9097e
SavchynNastya <104074388+SavchynNastya@users.noreply.github.com>
2022-07-16 18:31:36 +0300

    rendering changed

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ root.render(
 reportWebVitals();
 */
 import React from "react";
+import { createRoot } from "react-dom/client";
 import ReactDOM from "react-dom/client";
 import WeatherApp from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -29,7 +30,12 @@ function App() {
     </div>
   );
 }
-
+/*
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
 reportWebVitals();
+*/
+
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
