be1f1a2cbd185ec28f1ed5ba734b0dc8ed44b37b
SavchynNastya <104074388+SavchynNastya@users.noreply.github.com>
2022-07-16 13:17:05 +0300

    react no functional app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -14,4 +15,21 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+*/
+import React from "react";
+import ReactDOM from "react-dom/client";
+import WeatherApp from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 reportWebVitals();
